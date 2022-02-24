70664bf04024cfe99a9cd642a6786dc010110fb6
Sean Cruz <seankennethcruz@gmail.com>
2022-02-24 11:34:38 -0800

    code

---
+++ Develop/Assets/js/script.js
@@ -0,0 +1,16 @@
+// variables for DOM elements
+let timeDisplayEl = $('#time-display');
+let projectDisplayEl = $('#project-display')
+let projectModelEL = $('#project-model')
+let projectFormEl = $('#project-form')
+let projectNameInputEl = $('#project-name-input')
+let projectTypeInputEl = $('#project-type-input')
+let hourlyRateInputEl = $('#hourly-rate-input')
+let dueDateInputEl = $('#due-date-input')
+
+// displaying the time
+function displayTime() {
+  let rightNow = moment().format('MMMM DD, YYYY [at] hh:mm:ss a')
+  timeDisplayEl.text(rightNow)
+}
+
